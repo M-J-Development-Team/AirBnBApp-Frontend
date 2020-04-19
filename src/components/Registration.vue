4be020591e44911f60109ec947658960ad729f2f
Mina Maras <minamaras1997@gmail.com>
2020-04-19 17:00:39 +0200

    Changed html for registration

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,28 +1,21 @@
 <template>
   <div class="registration">
-    <h2>Registration</h2>
+    <img  src="../assets/sign-up.svg" alt="Smiley face" height="42" width="42">
+    <div class="whitespace"></div>
     <form>
-      <div class="row">
-        <input type="text" class="input-first" placeholder="Name" required />
-        <input type="text" class="input-first" placeholder="Lastname" required />
-        <select name="gender" id="gender">
-            <option value="Female">Female</option>
-            <option value="Male">Male</option>
-            <option value="RatherNotSay">Rather Not Say</option>
-        </select>
+      <b-form-input v-model="name" type=text class="inputs" placeholder="Enter your name"></b-form-input>
+      <b-form-input v-model="lastname" type=text class="inputs" placeholder="Enter your lastanme"></b-form-input>
+      <b-form-select v-model="gender" class="genderSelect" :options="options"></b-form-select>
+      <br/>
+      <br/>
+      <b-form-input v-model="username" type=text class="inputs" placeholder="Enter your username"></b-form-input>
+      <b-form-input v-model="password" type=password class="inputs" placeholder="Enter your password"></b-form-input>
+      <b-form-input v-model="repeatedpassword" type=password class="inputs" placeholder="Repeat password"></b-form-input>
+      <br/>
 
-      </div>
-      <br />
-      <div class="profil">
-        <input type="text" class="input-username" placeholder="Username" required />
-        <input type="password" class="input-pass" placeholder="Password" required />
-        <input type="password" class="input-pass" placeholder="Repeat Password" required />
-      </div>
-      <div class="dugmad">
-        <input class="dugme1" type="submit" value="Register">
-        <button class="dugme2">Cancel</button> 
-      </div>
+      <b-button class="btns" variant="outline-primary">Sign Up</b-button>
     </form>
+    
   </div>
 </template>
 
@@ -37,7 +30,13 @@ export default {
         username: "",
         password: ""
       },
-
+       selected: null,
+        options: [
+          { value: 'none', text: 'Please select an option' },
+          { value: 'female', text: 'Female' },
+          { value: 'male', text: 'Male' },
+          { value: 'rathernotsay', text: 'Rather Not Say' }
+        ],
       headers: {
         "Content-Type": "application/json"
       }
@@ -51,6 +50,7 @@ h2{
     text-align: center;
     color: #5e565e;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    margin-top: 5px;
 }
 
 #gender {
@@ -63,12 +63,23 @@ h2{
 
 .registration{
     padding: 30px;
-    background-color: #eee5f3;
+    background-color: #d9f2fc;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translateX(-50%) translateY(-50%);
     box-shadow: 0px 10px 17px rgba(0, 0, 0, 0.1);
+}
+
+.btns {
+  margin: 10px;
+  border-color: #06a757;
+  color: #06a757;
+}
+
+.btns:hover {
+  background-color: #06a757;
+  color: #d9f2fc;
 }
 
 .row {
@@ -78,6 +89,10 @@ h2{
   padding: 5px;
 }
 
+.whitespace {
+  padding: 7px;
+}
+
 .dugmad{
   display: flex;
   flex-direction: row;
@@ -85,14 +100,8 @@ h2{
   align-content: center;
 }
 
-.input-first{
-    margin-left: 50px;
-    padding: 7px;
-    width: 176px;
-    height: 15px;
-    font-size: 15px;
-    border: 1px solid #baa1c9;
-    border-radius: 18px;
+.inputs {
+  margin: 7px 1px;
 }
 
 .input-username, .input-pass{
