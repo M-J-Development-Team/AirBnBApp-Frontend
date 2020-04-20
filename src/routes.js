78ea9e2c9f56264f3e5269e883dd132960ba091f
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-21 00:10:07 +0200

    added new routes

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,16 @@ import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Navbar from './components/Navbar.vue'
 import Registration from './components/Registration.vue'
+import MyInfo from './components/MyInfo.vue'
+import AllUsers from './components/AllUsers.vue'
+
 
 
 export default[
     { path:'/', components: {default: Home, header: Navbar}},
     { path: '/login', component: Login},
     { path: '/registration', component: Registration},
+    { path: '/myinfo', component: MyInfo},
+    { path: '/allusers', component: AllUsers},
     
 ]
