47993579749c3448f5940f896a502a6f5bde3ab8
Mina Maras <minamaras1997@gmail.com>
2020-04-20 12:33:10 +0200

    added logout and get user info only on front

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,7 @@
         <router-link class="links" to="/"  v-tooltip.hover title="Home"><img src="../assets/homeicon.png" class="icon"/></router-link>
         <router-link class="links" to="/login" v-if="!this.$session.exists()" v-tooltip.hover title="Login">Login</router-link>
         <router-link class="links" to="/registration" v-if="!this.$session.exists()" v-tooltip.hover title="Registration">Registration</router-link>
+        <button v-if="this.$session.exists()" v-on:click.prevent="logout">Log Out</button>
         </nav>
     
     </div>
@@ -22,9 +23,49 @@ export default {
    
   data() {
     return {
-      //
-    }
+     role: "",
+        host: false,
+        admin: false,
+        guest: false,
+        headers : {
+          'Content-Type' : 'application/json'
+        }
+      }
+    },
+
+    methods : {
+    logout : function() {
+      this.$http.post('http://localhost:8080/PocetniREST/rest/logout', this.$session.get('idOne') ,{headers:this.headers}).then((response) => {
+        console.log(response);
+        this.$emit('logOut');
+        this.$session.destroy();
+        this.$router.push('/')
+        
+        }, () =>{
+            this.swal("Logged Out!")
+        })
+    },
   },
+
+  created(){
+    if(this.$session.exists()){
+    this.$http.post('http://localhost:8080/PocetniREST/rest/userinfo', this.$session.get('idOne') ,{headers:this.headers}).then((response) => {
+      this.role = response.body.role;
+      if(this.role === "HOST"){
+        this.host = true;
+      } else if (response.body.role === "ADMIN"){
+        this.admin = true;
+      } else if (response.body.role === "GUEST"){
+        this.guest = true;
+      }
+    })
+      
+  }
+}
+
+
+
+
  
 
 }
