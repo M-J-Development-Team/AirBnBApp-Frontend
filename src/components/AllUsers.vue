b81b59f6665ae5a2ca61e388599ccf76596e98b4
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-21 00:06:31 +0200

    added component for showing admin all users

---
+++ src/components/AllUsers.vue
@@ -0,0 +1,30 @@
+<template>
+
+        
+  
+</template>
+
+<script>
+export default {
+  name: "AllUsers",
+  data() {
+    return {
+      users:[],
+      headers : {
+        'Content-Type' : 'application/json'
+      }
+    }
+  },
+
+  methods : {
+
+      
+  
+  }
+
+};
+</script>
+
+<style scoped>
+
+</style>
