a7f62d7a1d6871cb23847beb0aa1d8ebd730019f
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-20 10:58:22 +0200

    added route for registration

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@
 import Login from './components/Login.vue'
 import Home from './components/Home.vue'
 import Navbar from './components/Navbar.vue'
+import Registration from './components/Registration.vue'
 
 
 export default[
     { path:'/', components: {default: Home, header: Navbar}},
     { path: '/login', component: Login},
+    { path: '/registration', component: Registration},
     
 ]
