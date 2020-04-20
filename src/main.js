d22362204dad63e49fa0b2d75135dd569dbb9293
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-20 10:09:37 +0200

    added imports

--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueSession from 'vue-session'
 import './plugins/bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 
-
+Vue.use(VueSweetalert2);
 Vue.use(BootstrapVue)
 Vue.use(VueRouter);
 Vue.use(VueResource);
