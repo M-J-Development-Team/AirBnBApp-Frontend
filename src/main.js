b496df218591d514928f136fcd2f2706af6041d5
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-20 10:03:41 +0200

    added imports

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import Routes from './routes'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSession from 'vue-session'
 
-Vue.config.productionTip = false
+
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.use(VueSession);
+
+
+
+const router = new VueRouter({
+  routes: Routes,
+  mode: 'history'
+});
+
+
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router: router
+})
