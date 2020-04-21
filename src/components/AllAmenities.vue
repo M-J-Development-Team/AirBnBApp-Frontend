230e364442eeeb5a9cee9fd00780a20e657653d0
Mina Maras <minamaras1997@gmail.com>
2020-04-21 19:06:39 +0200

    Added all amenities component

---
+++ src/components/AllAmenities.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "AllAmenities",
+    data() {
+        return {
+            amenities: "",
+            headers : {
+          'Content-Type' : 'application/json'
+        }
+
+        }
+    },
+
+    methods : {
+      
+  
+  }
+
+    
+}
+</script>
+
+<style scoped>
+
+
+</style>
