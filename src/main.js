a6a32369527c408dc4e900ae119d306048f56ef6
jelena-bojanic <56951237+jelena-bojanic@users.noreply.github.com>
2020-04-23 00:20:30 +0200

    updates

--- src/main.js
+++ src/main.js
@@ -11,12 +11,25 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueSession from 'vue-session'
 import './plugins/bootstrap-vue'
 import VueSweetalert2 from 'vue-sweetalert2';
+import Geocoder from "@pderas/vue2-geocoder";
+import 'vue-multiselect/dist/vue-multiselect.min.css';
+ 
+
+
+Vue.use(Geocoder, {
+    defaultCountryCode: null, // e.g. 'CA'
+    defaultLanguage:    null, // e.g. 'en'
+    defaultMode:        'address', // or 'lat-lng'
+    googleMapsApiKey:   'af06866a8emsh40e59efd21a2a76p1c43bejsn44800aec1b62'
+});
 
 Vue.use(VueSweetalert2);
 Vue.use(BootstrapVue)
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(VueSession);
+Vue.use(require('vue-moment'));
+
 
 
 
